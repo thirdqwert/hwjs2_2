185dae71661056339121e6397278659d91a19138
qwert <qwert@gmail.com>
2024-03-14 02:48:56 +0500

    firstMaster

---
+++ main.js
@@ -0,0 +1,30 @@
+do {
+    var numberIpn = +prompt('Введите число')
+} while (isNaN(numberIpn) || degree == 0)
+    var numberSecond = numberIpn
+do {
+    var degree = +prompt('Введи степень')
+} while (isNaN(degree) || degree == 0)
+
+for (let i = 1; i < degree; i++) {
+    numberIpn = numberIpn * numberSecond
+}
+alert(numberIpn);
+
+
+do {
+    var steps = +prompt('Введите число ступенук')
+} while (isNaN(steps) || steps == 0);
+do {
+    var middle = prompt('Ведите первый символ')
+} while (!isNaN(middle) || middle == '');
+do {
+    var last = prompt('Ведите второй символ')
+} while (!isNaN(last) || last == '');
+var secondMiddle = middle
+console.log(last);
+for (let i = 1; i < steps; i++) {
+    console.log( middle + last);
+    middle = middle + secondMiddle
+
+}
